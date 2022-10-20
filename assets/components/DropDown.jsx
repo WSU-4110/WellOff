cb72a8d19a540d702a6cd0cbd0453aa5c763da97
Matthew Meyer <matt.meyer997@gmail.com>
2022-10-20 00:56:15 -0400

    Fixed code styling in DropDown.

--- assets/components/DropDown.jsx
+++ assets/components/DropDown.jsx
@@ -1,19 +1,19 @@
-import React, { useState  }from 'react';
+import React, { useState } from 'react';
 import { View, Text } from 'react-native';
 import SelectList from 'react-native-dropdown-select-list';
 import style from '../../styles'
 
-export default function DropDown(props){
+export default function DropDown(props) {
     const [selected, setSelected] = React.useState("");
 
-    return(
-            <View style={style.switchContainer}>
-                <Text>{props.name}</Text>
-                <SelectList setSelected={setSelected} 
-                    data={props.data}  
-                    search={false} 
-                    defaultOption={props.data[0]}
-                />
-            </View>
+    return (
+        <View style={style.switchContainer}>
+            <Text>{props.name}</Text>
+            <SelectList setSelected={setSelected}
+                data={props.data}
+                search={false}
+                defaultOption={props.data[0]}
+            />
+        </View>
     );
 };

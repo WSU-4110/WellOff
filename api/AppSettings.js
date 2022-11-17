be2b724a0e1fed17aa36fbf7a6d4b83e8dc6d524
Matthew Meyer <matt.meyer997@gmail.com>
2022-11-17 16:39:42 -0500

    Created app settings with a few functions that are not fully impl.

---
+++ api/AppSettings.js
@@ -0,0 +1,43 @@
+import GoogleFit, { Scopes } from 'react-native-google-fit'
+
+var express = require('express');
+var router = express.Router();
+
+const options = {
+    scopes: [
+      Scopes.FITNESS_ACTIVITY_READ,
+      Scopes.FITNESS_ACTIVITY_WRITE,
+      Scopes.FITNESS_BODY_READ,
+      Scopes.FITNESS_BODY_WRITE,
+    ],
+  }
+
+router.get('/dark-mode-status', function(req, res){
+    res.json({status: darkModeEnabled});
+});
+
+router.get('/google-fit-status', function(req, res){
+    res.json(GoogleFit.isAuthorized)
+});
+
+router.set('/set-google-fit-status', function(req, res){
+    if (!req.body.enabled){
+        res.status(400);
+        res.json({message: "Bad Request"});
+    }
+    else {
+        GoogleFit.authorize(options).then(authResult => {
+            if (authResult.success) {
+                dispatch("AUTH_SUCCESS");
+            } else {
+                dispatch("AUTH_DENIED", authResult.message);
+            }
+        })
+            .catch(() => {
+                dispatch("AUTH_ERROR");
+            })
+    }
+});
+
+//Routes will go here
+module.exports = router;

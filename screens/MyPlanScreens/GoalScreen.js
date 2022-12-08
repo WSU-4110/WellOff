f063a558ea6a38412fc25a0db13462c33e7333bd
Raja Moussa <rajamoussa09@outlook.com>
2022-12-08 00:39:58 -0500

    Created Edit Goals Page and added to navi

---
+++ screens/MyPlanScreens/GoalScreen.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const GoalScreen = () => {
+    return (
+        <View style = {styles.container}>
+            <View style={styles.space} />
+            <View style={styles.space} />
+            <View style={styles.space} />
+            <View style={styles.space} />
+            <View style={styles.space} />
+            <View style={styles.space} />
+            <View style={styles.space} />
+            <View style={styles.space} />
+            <View style={styles.space} />
+            <View style={styles.space} />
+            <View style={styles.space} />
+            <View style={styles.space} />
+            <View style={styles.space} />
+            <View style={styles.space} />
+            <View style={styles.space} />
+            <View style={styles.space} />
+            <View style={styles.space} />
+            <View style={styles.space} />
+            <View style={styles.space} />
+            <View style={styles.space} />
+            <View style={styles.space} />
+            <View style={styles.space} />
+            <View style={styles.space} />
+            <View style={styles.space} />
+            <View style={styles.space} />
+            <View style={styles.space} />
+            <View style={styles.space} />
+            <View style={styles.space} />
+            <View style={styles.space} />
+            <View style={styles.space} />
+            <View style={styles.space} />
+            <View style={styles.space} />
+            <View style={styles.space} />
+            <View style={styles.space} />
+            <View style={styles.space} />
+            <View style={styles.space} />
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    container: {
+      backgroundColor: '#D4F1F4',
+      justifyContent: 'center'
+    },
+
+    space: {
+        padding: 4,
+    }
+});
+
+export default GoalScreen;

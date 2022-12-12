78fc8eb52a82a926ad364acca202d0a166951856
rifrod <rifattrodoshi@gmail.com>
2022-12-12 18:14:18 -0500

    Test cases to test 6 different methods for unit testing

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -5,10 +5,43 @@
 import 'react-native';
 import React from 'react';
 import App from '../App';
+import {
+  __getCompletedIcon,
+  __getDescriptionStyle,
+  clickEventListener,
+  onClickListener,
+  __renderColors,
+  __renderImages,
+} from '../TestCasses';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+test('get icons', () => {
+  const icon = 'https://img.icons8.com/flat_round/64/000000/delete-sign.png';
+  expect(__getCompletedIcon(2)).toEqual(icon);
+});
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+test('geting styles', () => {
+  const style = {
+    textDecorationLine: 'line-through',
+    fontStyle: 'italic',
+    color: '#808080',
+  };
+  expect(__getDescriptionStyle(1)).toEqual(style);
+});
+
+test('click EventListner', () => {
+  expect(clickEventListener()).toBeUndefined();
+});
+
+test('render colors', () => {
+  const tree = __renderColors();
+  expect(tree).toMatchSnapshot();
+});
+
+test('render Images', () => {
+  const tree = __renderColors();
+  expect(tree).toMatchSnapshot();
+});
+
+test('Button pressed', () => {
+  expect(onClickListener()).toBeUndefined();
 });

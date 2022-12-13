baf8fcc064a2538cb3d46407e3bf2d1aa120c9d2
Raja Moussa <rajamoussa09@outlook.com>
2022-12-12 20:59:56 -0500

    Assignment 5 Raja Moussa Unit Testing

---
+++ Raja_UnitTesting.js
@@ -0,0 +1,25 @@
+// Raja Moussa Assignment 5 Unit Testing
+
+export const Test1 = (a, b) => {
+    return a + b;
+}
+
+export const Test2 = (a, b) => {
+    return a - b;
+}
+
+export const Test3 = (a, b) => {
+    return a * b;
+}
+
+export const Test4 = (a, b) => {
+    return a / b;
+}
+
+export const Test5 = (a, b) => {
+    return a % b;
+}
+
+export const Test6 = (a, b, c) => {
+    return a + b + c;
+}
--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -4,11 +4,34 @@
 
 import 'react-native';
 import React from 'react';
+import { Test1, Test2, Test3, Test4, Test5, Test6 } from '../Raja_UnitTesting';
 import App from '../App';
-
-// Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
+it('Practice Test', () => {
   renderer.create(<App />);
+});
+
+test('Test 1', () => {
+  expect(Test1(1, 2)).toBe(3);
+});
+
+test('Test 2', () => {
+  expect(Test2(4, 2)).toBe(2);
+});
+
+test('Test 3', () => {
+  expect(Test3(5, 2)).toBe(10);
+});
+
+test('Test 4', () => {
+  expect(Test4(10, 2)).toBe(5);
+});
+
+test('Test 5', () => {
+  expect(Test5(9, 2)).toBe(1);
+});
+
+test('Test 6', () => {
+  expect(Test6(1, 2, 3)).toBe(6);
 });

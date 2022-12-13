be8745f33d1aaac934fca1d6c42496e8833592cf
Matthew Meyer <37270280+commandobox@users.noreply.github.com>
2022-12-12 21:31:40 -0500

    Added further documentation.

--- math.js
+++ math.js
@@ -1,6 +1,7 @@
 //A representation of the api.js file I created where functions are called and normal values are returned.
+//Link: https://github.com/WSU-4110/WellOff/blob/48feafd6e916f6e23c572772d52e8b27316a5674/api/api.js
 //Created by: Matthew Meyer
-//Contributor(s): Matthew Meyer
+//Contributors: Matthew Meyer
 
 /**
  * Returns a + b

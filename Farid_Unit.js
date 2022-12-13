9a03396d152a4a0a04389583cabf5f76cd2a4689
FredddyAdu <baalbakifarid@gmail.com>
2022-12-12 22:00:14 -0500

    Finished Test Code

---
+++ Farid_Unit.js
@@ -0,0 +1,23 @@
+export const App1 = (a, b) => {
+    return a + b;
+}
+
+export const App2 = (a, b) => {
+    return a - b;
+}
+
+export const App3 = (a, b) => {
+    return a * b;
+}
+
+export const App4 = (a, b) => {
+    return a / b;
+}
+
+export const App5 = (a, b) => {
+    return a % b;
+}
+
+export const App6 = (a, b, c) => {
+    return a + b + c;
+}
--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -4,11 +4,35 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../App';
-
+import App from '../App'
+import { App1, App2, App3, App4, App5, App6 } from '../Farid_Unit';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
   renderer.create(<App />);
+});
+
+test('Test 1', () => {
+  expect(App1(4, 5)).toBe(9);
+});
+
+test('Test 2', () => {
+  expect(App2(10, 3)).toBe(7);
+});
+
+test('Test 3', () => {
+  expect(App3(5, 4)).toBe(20);
+});
+
+test('Test 4', () => {
+  expect(App4(20, 2)).toBe(10);
+});
+
+test('Test 5', () => {
+  expect(App5(9, 3)).toBe(0);
+});
+
+test('Test 6', () => {
+  expect(App6(4, 2, 3)).toBe(9);
 });

8a50a0bd67dcb9dc4971c1f15f1389e3c9eee154
Matthew Meyer <37270280+commandobox@users.noreply.github.com>
2022-12-12 21:50:27 -0500

    Added documentation to each of my test cases.

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -29,60 +29,102 @@ afterAll(() => {
   randNum = null;
 })
 
+/**
+ * @author Matthew Meyer
+ */
 test('App renders correctly', () => {
   renderer.create(<App />);
 });
 
+/**
+ * @author Matthew Meyer
+ */
 test('Text renders correclty', () => {
   renderer.create(<Text>test</Text>);
 });
 
+/**
+ * @author Matthew Meyer
+ */
 test('Date renders correctly', () => {
   renderer.create(<Date value="11/12/2022"/>);
 });
 
+/**
+ * @author Matthew Meyer
+ */
 test ('Add 1 and 2', ()=>{
   expect(add(num2, num3)).toEqual(3);
 });
 
+/**
+ * @author Matthew Meyer
+ */
 test ('Add 2 and 1', ()=>{
   expect(add(num3, num2)).toEqual(3);
 });
 
+/**
+ * @author Matthew Meyer
+ */
 test ('Add 0 and 1', () => {
   expect(add(num1, num2)).toEqual(num2);
 });
 
+/**
+ * @author Matthew Meyer
+ */
 test ('Add 0 and a random number', () => {
   expect(add(num1, randNum)).toEqual(randNum);
 });
 
+/**
+ * @author Matthew Meyer
+ */
 test('Divide 1 and 0', () => {
   expect(() => (div(num2, num1))).toThrow();
 });
 
+/**
+ * @author Matthew Meyer
+ */
 test('Divide by random number and zero.', () => {
   const error = "Divisor cannot be 0";
   expect(() => div( randNum, num1)).toThrow(error);
 });
 
+/**
+ * @author Matthew Meyer
+ */
 test('Divide by 1 and 2', () => {
   expected = 0.5;
   expect(div(1, 2)).toBeCloseTo(expected);
 });
 
+/**
+ * @author Matthew Meyer
+ */
 test('Divide by 2 and 1', () => {
   expect(div(1, 2)).toBeCloseTo(0.5);
 });
 
+/**
+ * @author Matthew Meyer
+ */
 test('Absoulte value of 1', () => {
   expect(abs(num2)).toEqual(num2);
 });
 
+/**
+ * @author Matthew Meyer
+ */
 test('Absoulte value of 0', () => {
   expect(abs(num1)).toEqual(num1);
 });
 
+/**
+ * @author Matthew Meyer
+ */
 test('Absoulte value of -1', () => {
   expect(abs(-1)).toEqual(num2);
 });

fcfb04821b05c9e7571bed595e6e5da53d5a88b4
rmkat <rmkatbey@gmail.com>
2022-12-12 19:20:46 -0500

    Unit Testing

---
+++ HelloWorld.js
@@ -0,0 +1,44 @@
+/* eslint-disable prettier/prettier */
+
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+export const App1 = () => {
+    return(
+    <Text>Hello World!</Text>
+    );
+    };
+    
+export const App2 = () => {
+      return(
+      <Text>Hello World! 2</Text>
+      );
+      };
+    
+export  const App3 = () => {
+        return(
+        <Text>Hello World! 3</Text>
+        );
+        };
+ export  const App4 = () => {
+          return(
+          <Text>Hello World! 4</Text>
+        );
+    
+    };
+
+    export  const App5 = () => {
+        return(
+        <Text>Hello World! 5</Text>
+      );
+  
+  };
--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 /**
  * @format
  */
@@ -5,6 +6,7 @@
 import 'react-native';
 import React from 'react';
 import App from '../App';
+import { App1, App2, App3, App4, App5 } from '../HelloWorld';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
@@ -12,3 +14,25 @@ import renderer from 'react-test-renderer';
 it('renders correctly', () => {
   renderer.create(<App />);
 });
+
+it('renders correctly app 1', () => {
+  renderer.create(<App1 />);
+});
+
+it('renders correctly app 2', () => {
+  renderer.create(<App2 />);
+});
+
+it('renders correctly app 3', () => {
+  renderer.create(<App3 />);
+});
+
+it('renders correctly app 4', () => {
+  renderer.create(<App4 />);
+});
+
+it('renders correctly app 5', () => {
+  renderer.create(<App5 />);
+});
+
+

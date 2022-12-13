0298e098af2e5860783d093849a0e962c2a19a9e
Matthew Meyer <37270280+commandobox@users.noreply.github.com>
2022-12-12 21:32:17 -0500

    Fixed tests and added private variables.

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -5,11 +5,27 @@
 import 'react-native';
 import App from '../App';
 import { Text } from 'react-native';
-import {expect, jest, test} from '@jest/globals';
+import {expect, jest, test, beforeAll} from '@jest/globals';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 import {add, div, abs} from '../math';
+
+var num1, num2, num3, randMax;
+
+beforeAll(() => {
+  num1 = 0;
+  num2 = 1;
+  num3 = 2;
+  randMax = 10;
+});
+
+afterAll(() => {
+  num1 = null;
+  num2 = null;
+  num3 = null;
+  randMax = null;
+})
 
 test('App renders correctly', () => {
   renderer.create(<App />);
@@ -24,16 +40,17 @@ test('Date renders correctly', () => {
 });
 
 test('Divide 1 and 0', () => {
-  expect(() => (div(1, 0))).toThrow();
+  expect(() => (div(num2, num1))).toThrow();
 });
 
 test('Divide by random number and zero.', () => {
   const error = "Divisor cannot be 0";
-  //expect(div( Math.floor( Math.random() * 100), 0)).toThrow(error);
+  expect(() => div( Math.floor( Math.random() * randMax), num1)).toThrow(error);
 });
 
 test('Divide by 1 and 2', () => {
-  expect(div(1, 2)).toBeCloseTo(0.5);
+  expected = 0.5;
+  expect(div(1, 2)).toBeCloseTo(expected);
 });
 
 test('Divide by 2 and 1', () => {

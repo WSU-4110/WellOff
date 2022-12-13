eb5e8509f02086ff7529509b935f064d04a994de
Matthew Meyer <37270280+commandobox@users.noreply.github.com>
2022-12-12 21:49:00 -0500

    Added randNum to the private variables and added test cases for the addition function.

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -11,13 +11,14 @@ import {expect, jest, test, beforeAll} from '@jest/globals';
 import renderer from 'react-test-renderer';
 import {add, div, abs} from '../math';
 
-var num1, num2, num3, randMax;
+var num1, num2, num3, randMax, randNum;
 
 beforeAll(() => {
   num1 = 0;
   num2 = 1;
   num3 = 2;
   randMax = 10;
+  randNum = Math.floor( Math.random() * randMax);
 });
 
 afterAll(() => {
@@ -25,6 +26,7 @@ afterAll(() => {
   num2 = null;
   num3 = null;
   randMax = null;
+  randNum = null;
 })
 
 test('App renders correctly', () => {
@@ -39,13 +41,29 @@ test('Date renders correctly', () => {
   renderer.create(<Date value="11/12/2022"/>);
 });
 
+test ('Add 1 and 2', ()=>{
+  expect(add(num2, num3)).toEqual(3);
+});
+
+test ('Add 2 and 1', ()=>{
+  expect(add(num3, num2)).toEqual(3);
+});
+
+test ('Add 0 and 1', () => {
+  expect(add(num1, num2)).toEqual(num2);
+});
+
+test ('Add 0 and a random number', () => {
+  expect(add(num1, randNum)).toEqual(randNum);
+});
+
 test('Divide 1 and 0', () => {
   expect(() => (div(num2, num1))).toThrow();
 });
 
 test('Divide by random number and zero.', () => {
   const error = "Divisor cannot be 0";
-  expect(() => div( Math.floor( Math.random() * randMax), num1)).toThrow(error);
+  expect(() => div( randNum, num1)).toThrow(error);
 });
 
 test('Divide by 1 and 2', () => {
